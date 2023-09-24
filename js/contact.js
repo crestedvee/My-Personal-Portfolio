3aa8a98c5b84cc0a4b44043c3e150719d1aa127d
Ivan Ray <rnldvee1@gmail.com>
2023-09-24 02:02:50 +0100

    updated site to have responsive design with better UI

--- js/contact.js
+++ js/contact.js
@@ -11,46 +11,106 @@ let hr1 = document.getElementById("line");
 let mobileMenu = document.getElementsByClassName("dropdown")[0];
 let headerView = document.getElementsByClassName("header-nav")[0];
 let contactF = document.getElementsByClassName('contact');
+let headerName = document.getElementById('header-name');
+let navChild1 = document.getElementById('nav1');
+let navChild2 = document.getElementById('nav2');
+let navChild3 = document.getElementById('nav3');
+let contactSection = document.getElementById('contact');
+let footerSection = document.getElementById('footer');
+let selectElement = document.querySelector("#toggle-button2");
+let headerSec = document.getElementById("header");
+let copyRight = document.getElementById("copy-right");
+let input1 = document.getElementById("username");
+let input2 = document.getElementById("email");
+let input3 = document.getElementById("num");
+let input4 = document.getElementById("msg");
+let contactDiv = document.getElementById("contact-div");
+let contactNum = document.getElementById("fnum");
+let contactMail = document.getElementById("fmail");
+let contactAdd = document.getElementById("fadd");
 
-
-let changeMode = () => {
-  if(tBtn.innerHTML === "Dark Mode"){
-    sbody.style.backgroundColor = "black"
-    sbody.style.color = "white"
-    sbody.style.opacity = 0.7;
-    tBtn.innerHTML = "Light Mode";
-    fTitle.style.color = "white";
-    cForm.style.backgroundColor = "#FFFFFF";
-    submitBtn.style.backgroundColor = "#0892d0";
-    submitBtn.classList.add('HoverClass1');
-    submitBtn.style.color = "white";
-    lab1.style.color = "#282C35";
-    lab2.style.color = "#282C35";
-    lab3.style.color = "#282C35";
-    lab4.style.color = "#282C35";
-    hr1.classList.add("changeClass1");
-    hr1.classList.add("changeClass2");
-  }else {
-    sbody.style.backgroundColor = "white"
-    sbody.style.color = "black"
-    sbody.style.opacity = null;
-    tBtn.innerHTML = "Dark Mode"
-    fTitle.style.color = "black";
-    fTitle.style.color = "white";
-    cForm.style.backgroundColor = "#FFFFFF";
-    submitBtn.style.backgroundColor= "#0892d0";
-    submitBtn.style.Color= "#FFFFFF";
-    fTitle.style.color = "#282C35";
-  }
-}
-
-function changeVieMobileView(){
+function changeVieMobileView() {
   document.getElementById("header-nav").classList.add('classHeader-nav');
 }
 
 const changeView = () => {
-  headerView.classList.toggle('active');
+  if (headerView.classList.toggle('active')) {
+    contactSection.style.top = "16.1rem";
+    footerSection.style.top = "73rem";
+  } else {
+    contactSection.style.top = "8rem";
+    footerSection.style.top = "65rem";
+  }
 }
 
-tBtn.addEventListener("click", changeMode);
+selectElement.addEventListener("change", (e) => {
+  const value = e.target.value;
+
+  if (value === "light") {
+      sbody.style.backgroundColor = "white";
+      sbody.style.color = "black";
+      sbody.style.opacity = null;
+      headerSec.style.backgroundImage = "url(/images/img15.jpeg)";
+      footerSection.style.backgroundImage = "url(/images/img15.jpeg)";
+      contactSection.style.backgroundImage = "url(/images/img16.avif)";
+      fTitle.style.color = "#282C35";
+      headerName.style.color = "#011638";
+      navChild1.style.color = "#FFFFFF";
+      navChild2.style.color = "#FFFFFF";
+      navChild3.style.color = "#FFFFFF";
+      cForm.style.backgroundImage = "none";
+      cForm.style.backgroundColor = "ffffff";
+      contactDiv.style.background = "none";
+      contactDiv.style.backgroundColor = "#ffffff";
+      lab1.style.color = "#282C35";
+      lab2.style.color = "#282C35";
+      lab3.style.color = "#282C35";
+      lab4.style.color = "#282C35";
+      input1.style.backgroundColor = "#FFFFFF";
+      input2.style.backgroundColor = "#FFFFFF";
+      input3.style.backgroundColor = "#FFFFFF";
+      input4.style.backgroundColor = "#FFFFFF";
+      copyRight.style.color = "#FFFFFF";
+      submitBtn.style.backgroundColor = "#0892d0";
+      submitBtn.style.Color = "#FFFFFF";
+
+  } else {
+    sbody.style.color = "#f4dbd8";
+    sbody.style.backgroundColor = "#282C35";
+    sbody.style.opacity = "0.8";
+    headerSec.style.backgroundImage = "url(/images/img13.jpeg)";
+    footerSection.style.backgroundImage = "url(/images/img13.jpeg)";
+    contactSection.style.background = "none";
+    contactSection.style.backgroundColor = "#282C35";
+    fTitle.style.color = "#f4dbd8";
+    cForm.style.backgroundImage = "url(/images/img13.jpeg)";
+    submitBtn.style.backgroundColor = "#0892d0";
+    submitBtn.classList.add('HoverClass2');
+    submitBtn.style.color = "white";
+    lab1.style.color = "#f4dbd8";
+    lab2.style.color = "#f4dbd8";
+    lab3.style.color = "#f4dbd8";
+    lab4.style.color = "#f4dbd8";
+    input1.style.backgroundColor = "#f4dbd8";
+    input2.style.backgroundColor = "#f4dbd8";
+    input3.style.backgroundColor = "#f4dbd8";
+    input4.style.backgroundColor = "#f4dbd8";
+    contactDiv.style.backgroundColor = "#f4dbd8";
+    headerName.style.color = "#59d2fe";
+    navChild1.style.color = "#59d2fe";
+    navChild2.style.color = "#59d2fe";
+    navChild3.style.color = "#59d2fe";
+    selectElement.style.color = "#59d2fe";
+    navChild1.classList.add("menuClass1");
+    navChild2.classList.add("menuClass1");
+    navChild3.classList.add("menuClass1");
+    hr1.classList.add("changeClass1");
+    hr1.classList.add("changeClass2");
+    copyRight.style.color = "#59d2fe";
+    contactNum.style.color = "#282C35";
+    contactMail.style.color = "#282C35";
+    contactAdd.style.color = "#282C35";
+  }
+});
+
 mobileMenu.addEventListener("click", changeView);
--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 let sbody = document.querySelector("body");
+let divBody = document.getElementById("main-body");
 let tBtn = document.getElementById("toggle-button");
 let abtxt = document.getElementById("aboutMeTxt");
 let abtxt2 = document.getElementById("aboutMeTxt2");
@@ -7,40 +8,91 @@ let pSum1 = document.getElementById("p1");
 let pSum2 = document.getElementById("p2");
 let pSum3 = document.getElementById("p3");
 let pSum4 = document.getElementById("p4");
-let pdetail = document.getElementsByClassName("project-detail");
+let pdetail = document.getElementById("project-list");
 let mobileMenu = document.getElementsByClassName("dropdown")[0];
 let headerView = document.getElementsByClassName("header-nav")[0];
+let headerName = document.getElementById('header-name');
+let navChild1 = document.getElementById('nav1');
+let navChild2 = document.getElementById('nav2');
+let navChild3 = document.getElementById('nav3');
+let navChild4 = document.getElementById('nav4');
 let contactF = document.getElementsByClassName('contact');
+let mainSection = document.getElementById('section');
+let footerSection = document.getElementById('footer');
+let headerColor = document.getElementById('header');
+let selectElement = document.querySelector("#toggle-button2");
+let imageDiv = document.getElementById("about-me-img");
+let mySkills = document.getElementById("my-main-skills");
+let copyRight = document.getElementById("copy-right");
 
 
+function changeVieMobileView() {
+  document.getElementById("header-nav").classList.add('classHeader-nav');
+}
 
-let changeMode = () => {
-  if(tBtn.innerHTML === "Dark Mode"){
-    sbody.style.backgroundColor = "black"
-    sbody.style.color = "white"
-    sbody.style.opacity = 0.7;
-    tBtn.innerHTML = "Light Mode";
-    abtxt.style.color = "white";
-    abtxt2.style.color = "white";
-    abtxt3.style.color = "white";
+selectElement.addEventListener("change", (e) => {
+  const value = e.target.value;
 
-    pdetail.style.backgroundColor = "white";
-  }else {
-    sbody.style.backgroundColor = "white"
-    sbody.style.color = "black"
+  if (value === "light") {
+    
+    sbody.style.color = "#000000";
     sbody.style.opacity = null;
-    tBtn.innerHTML = "Dark Mode"
+    mainSection.style.backgroundImage = "url(/images/img16.avif)";
+    headerColor.style.backgroundImage = "url(/images/img15.jpeg)";
+    footerSection.style.backgroundImage = "url(/images/img15.jpeg)";
+    mySkills.style.backgroundImage = "url(/images/img15.jpeg)";
+    pdetail.style.backgroundImage = "none";
+    pdetail.style.backgroundImage = "url(/images/img15.jpeg)";
+    imageDiv.style.backgroundColor = "transparent";
     abtxt.style.color = "#282C35";
     abtxt2.style.color = "#282C35";
     abtxt3.style.color = "#282C35";
+    headerName.style.color = "#011638";
+    copyRight.style.color = "#011638";
+    navChild1.style.color = "#FFFFFF";
+    navChild2.style.color = "#FFFFFF";
+    navChild3.style.color = "#FFFFFF";
+    navChild4.style.color = "#FFFFFF";
+    selectElement.style.color = "#FFFFFF";
+    imageDiv.style.backgroundColor = "transparent";
+  } else {
+    sbody.style.color = "#f4dbd8";
+    sbody.style.backgroundColor = "#282C35";
+    sbody.style.opacity = "0.8";
+    headerColor.style.backgroundImage = "url(/images/img13.jpeg)";
+    footerSection.style.backgroundImage = "url(/images/img13.jpeg)";
+    mainSection.style.background = "none";
+    mainSection.style.backgroundColor = "#282C35";
+    mySkills.style.backgroundImage = "url(/images/img13.jpeg)";
+    abtxt.style.color = "#f4dbd8";
+    abtxt2.style.color = "#f4dbd8";
+    abtxt3.style.color = "#f4dbd8";
+    headerName.style.color = "#59d2fe";
+    navChild1.style.color = "#59d2fe";
+    navChild2.style.color = "#59d2fe";
+    navChild3.style.color = "#59d2fe";
+    navChild4.style.color = "#59d2fe";
+    selectElement.style.color = "#59d2fe";
+    copyRight.style.color = "#59d2fe";
+    navChild1.classList.add("menuClass1");
+    navChild2.classList.add("menuClass1");
+    navChild3.classList.add("menuClass1");
+    navChild4.classList.add("menuClass1");
+    pdetail.style.backgroundImage = "url(/images/img13.jpeg)";
+    imageDiv.style.backgroundColor = "transparent";
+  }
+});
 
+
+const changeView = () => {
+  if(headerView.classList.toggle('active')){
+    mainSection.style.top = "18.1rem";
+    footerSection.style.top = "192rem";
+  } else {
+    mainSection.style.top = "8rem";
+    footerSection.style.top = "182rem";
   }
 }
 
 
-const changeView = () => {
-  headerView.classList.toggle('active');
-}
-
-tBtn.addEventListener("click", changeMode);
 mobileMenu.addEventListener("click", changeView);
